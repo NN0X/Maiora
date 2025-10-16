2767a6332b091bd4a5760a396f1c6a1a95feeb64
nox <nox@noxsoftware.pl>
2025-10-16 20:27:25 +0200

    added node types

--- src/parser/ast.h
+++ src/parser/ast.h
@@ -5,11 +5,27 @@
 
 enum Ast
 {
+        AST_ROOT = 0,
+
+
         // --- KEYWORDS ---
 
-        AST_KEYW_RETURN = 0,
+        // modifiers
+        AST_KEYW_RETURN,
+        AST_KEYW_PUBLIC,
+        AST_KEYW_PRIVATE,
+        AST_KEYW_ENTRY,
 
-        
+        // functions
+        AST_KEYW_INSTANCE,
+        AST_KEYW_FUNCTION,
+
+        // memory
+        AST_KEYW_HEAP,
+        AST_KEYW_ADDRESS,
+        AST_KEYW_REFERENCE,
+        AST_KEYW_REF,
+        AST_KEYW_AT,
 
         // control flow
         AST_KEYW_WHILE,
@@ -21,6 +37,22 @@ enum Ast
 
 
         // --- OPERATIONS ---
+
+        // comparators
+        AST_OP_EQUALS,
+        AST_OP_LESS,
+        AST_OP_MORE,
+        AST_OP_LESS_EQUALS,
+        AST_OP_MORE_EQUALS,
+        AST_OP_NOT_EQUALS,
+        AST_OP_AND,
+        AST_OP_OR,
+        AST_OP_NOT,
+
+        // accessors
+        AST_OP_FROM,
+        AST_OP_IN,
+        AST_OP_FROM_MODULE,
 
         // modifiers
 
@@ -39,13 +71,47 @@ enum Ast
         AST_OP_BITWISE_AND,
 
 
+        // -- TYPES ---
+
+        AST_TYPE_NONE,
+        AST_TYPE_BOOL,
+
+        // integers
+        AST_TYPE_SINT8,
+        AST_TYPE_SINT16,
+        AST_TYPE_SINT32,
+        AST_TYPE_SINT64,
+        AST_TYPE_UINT8,
+        AST_TYPE_UINT16,
+        AST_TYPE_UINT32,
+        AST_TYPE_UINT64,
+
+        // floats
+        AST_TYPE_FLOAT8,
+        AST_TYPE_FLOAT16,
+        AST_TYPE_FLOAT32,
+        AST_TYPE_FLOAT64,
+
+        // characters
+        AST_TYPE_ASCII,
+        AST_TYPE_UTF8,
+
+
         // --- LITERALS ---
 
-
-        AST_LIT_BOOL,
         AST_LIT_INT,
+        AST_LIT_SINT,
+        AST_LIT_UINT,
+        AST_LIT_BOOL,
         AST_LIT_FLOAT,
+        AST_LIT_FLOAT32,
+        AST_LIT_FLOAT64,
         AST_LIT_CHAR,
+
+
+        // -- IDENTIFIERS ---
+
+        AST_ID,
 };
 
 typedef struct AstNode ANode_t;
@@ -53,11 +119,16 @@ typedef struct AstNode ANode_t;
 typedef struct AstNode
 {
         uint8_t type;
+
         uint64_t line;
         uint64_t pos;
+
         ANode_t* parent;
         ANode_t** children;
-        uint16_t numChildren;
+        uint64_t numChildren;
+
+        void* value;
+        uint64_t valueLen;
 } ANode_t;
 
 #endif // AST_H
