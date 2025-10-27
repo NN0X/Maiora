b803971bf22a108a4c4c75a3790f0714cbed5358
NoX <nox@noxsoftware.pl>
2025-10-27 13:17:15 +0100

    added getGroup prototype

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -1,13 +1,38 @@
+#include <stdio.h>
+
 #include "ast.h"
 #include "../lexer/lexer.h"
 
-ANTypes_t decideNodeType(LTok_t token)
+int getGroup(uint64_t* end, LTok_t* tokens, uint64_t numTokens, uint64_t pos)
+{
+        // TODO: find first group separated by ; or {
+        return 0;
+}
+
+ANTypes_t decideNodeType(uint64_t* pos, LTok_t* tokens, uint64_t numTokens)
 {
         return AST_INVALID;
 }
 
 int generateAST(LData_t lexerData, uint64_t numTokens, ANode_t* root)
 {
+        uint64_t pos = 0;
+        while (pos != numTokens)
+        {
+                uint64_t end;
+                if (getGroup(&end, lexerData.tokens, numTokens, pos) == 1)
+                {
+                        fprintf(stderr, "There are tokens with no ; or { to specify group end.");
+                        return 1;
+                }
+
+                ANTypes_t nodeType = decideNodeType(&pos, lexerData.tokens, end);
+                if (nodeType == AST_INVALID)
+                {
+                        fprintf(stderr, "Invalid AST Node");
+                        return 1;
+                }
+        }
 
         return 0;
 }
