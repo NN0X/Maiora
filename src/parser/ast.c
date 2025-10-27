0d44002b4676127e39816b546a267cdfedd69d6b
NoX <nox@noxsoftware.pl>
2025-10-27 18:28:30 +0100

    finishes getGroup

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -2,14 +2,32 @@
 
 #include "ast.h"
 #include "../lexer/lexer.h"
+#include "../lexer/token.h"
 
 int getGroup(uint64_t* end, LTok_t* tokens, uint64_t numTokens, uint64_t pos)
 {
-        // TODO: find first group separated by ; or {
+        uint64_t currPos = pos;
+        while (currPos != numTokens)
+        {
+                if (tokens[currPos] == TOK_OP_SEMICOLON || tokens[currPos] == TOK_OP_LCURLY)
+                {
+                        *end = currPos;
+                        return 0;
+                }
+                currPos++;
+        }
+
+        if (*end + 1 == numTokens && tokens[currPos] != TOK_OP_RCURLY)
+        {
+                fprintf(stderr, "Missing closing bracket.\n");
+                return 1;
+        }
+        *end = currPos;
+
         return 0;
 }
 
-ANTypes_t decideNodeType(uint64_t* pos, LTok_t* tokens, uint64_t numTokens)
+ANTypes_t decideNodeType(uint64_t pos, LTok_t* tokens, uint64_t numTokens)
 {
         return AST_INVALID;
 }
@@ -22,16 +40,17 @@ int generateAST(LData_t lexerData, uint64_t numTokens, ANode_t* root)
                 uint64_t end;
                 if (getGroup(&end, lexerData.tokens, numTokens, pos) == 1)
                 {
-                        fprintf(stderr, "There are tokens with no ; or { to specify group end.");
+                        fprintf(stderr, "Missing statement closing symbol: ; {  }.\n");
                         return 1;
                 }
 
-                ANTypes_t nodeType = decideNodeType(&pos, lexerData.tokens, end);
+                ANTypes_t nodeType = decideNodeType(pos, lexerData.tokens, end);
                 if (nodeType == AST_INVALID)
                 {
                         fprintf(stderr, "Invalid AST Node");
                         return 1;
                 }
+                pos = end;
         }
 
         return 0;
