45414b7b859ef277e63e2972f8554dc67ee8d732
nox <nox@noxsoftware.pl>
2025-10-27 22:11:43 +0100

    added genASTPass and example.mai

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -9,7 +9,7 @@ int getGroup(uint64_t* end, LTok_t* tokens, uint64_t numTokens, uint64_t pos)
         uint64_t currPos = pos;
         while (currPos != numTokens)
         {
-                if (tokens[currPos] == TOK_OP_SEMICOLON || tokens[currPos] == TOK_OP_LCURLY)
+                if (tokens[currPos].token == TOK_OP_SEMICOLON || tokens[currPos].token == TOK_OP_LCURLY)
                 {
                         *end = currPos;
                         return 0;
@@ -17,7 +17,7 @@ int getGroup(uint64_t* end, LTok_t* tokens, uint64_t numTokens, uint64_t pos)
                 currPos++;
         }
 
-        if (*end + 1 == numTokens && tokens[currPos] != TOK_OP_RCURLY)
+        if (*end + 1 == numTokens && tokens[currPos].token != TOK_OP_RCURLY)
         {
                 fprintf(stderr, "Missing closing bracket.\n");
                 return 1;
@@ -29,10 +29,14 @@ int getGroup(uint64_t* end, LTok_t* tokens, uint64_t numTokens, uint64_t pos)
 
 ANTypes_t decideNodeType(uint64_t pos, LTok_t* tokens, uint64_t numTokens)
 {
+        // INFO: starts with public/private/entry and contains ( -> function declaration
+        // INFO: starts with public/private or type and contains = -> variable declaration
+        // INFO: check if it's a statement
+        // INFO: if none of the above, it's an expression
         return AST_INVALID;
 }
 
-int generateAST(LData_t lexerData, uint64_t numTokens, ANode_t* root)
+int generateASTPass(LData_t lexerData, uint64_t numTokens, ANode_t* root)
 {
         uint64_t pos = 0;
         while (pos != numTokens)
@@ -44,6 +48,11 @@ int generateAST(LData_t lexerData, uint64_t numTokens, ANode_t* root)
                         return 1;
                 }
 
+                if (end == pos)
+                {
+                        break;
+                }
+
                 ANTypes_t nodeType = decideNodeType(pos, lexerData.tokens, end);
                 if (nodeType == AST_INVALID)
                 {
@@ -51,6 +60,36 @@ int generateAST(LData_t lexerData, uint64_t numTokens, ANode_t* root)
                         return 1;
                 }
                 pos = end;
+        }
+
+        return 0;
+}
+
+int getASTSize(uint64_t* size, ANode_t* root)
+{
+        // INFO: traverse the AST and count nodes
+        return 0;
+}
+
+int generateAST(LData_t lexerData, uint64_t numTokens, ANode_t* root)
+{
+        // INFO: call generateASTPass until no changes are made
+
+        uint64_t size = 1;
+        uint64_t prevSize = 0;
+        while (size != prevSize)
+        {
+                prevSize = size;
+                if (generateASTPass(lexerData, numTokens, root) == 1)
+                {
+                        fprintf(stderr, "Failed to generate AST.\n");
+                        return 1;
+                }
+                if (getASTSize(&size, root) == 1)
+                {
+                        fprintf(stderr, "Failed to get AST size.\n");
+                        return 1;
+                }
         }
 
         return 0;
