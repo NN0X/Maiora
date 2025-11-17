216ad5e6996a4d5ad578f7a3b1b88c2fc580ee11
NoX <nox@noxsoftware.pl>
2025-11-17 13:47:36 +0100

    started creating error module

---
+++ src/errors.h
@@ -0,0 +1,19 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+typedef enum PreprocessorErrors
+{
+        PRE_ERR_UNKNOWN = -1,
+} PrErr_t;
+
+typedef enum LexerErrors
+{
+        LEX_ERR_UNKNOWN = -1,
+} LErr_t;
+
+typedef enum ParserErrors
+{
+        PAR_ERR_UNKNOWN = -1,
+} PaErr_t;
+
+#endif
